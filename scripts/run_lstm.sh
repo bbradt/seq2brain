722f37b5e70b28ba7109db7b4378772fd1fa27e1
Brad <bbradt@gmail.com>
2019-10-28 18:19:57 +0000

    trying github LFS

--- scripts/run_lstm.sh
+++ scripts/run_lstm.sh
@@ -1,4 +1,4 @@
 mkdir -p checkpoints
 CUDA_VISIBLE_DEVICES=0 fairseq-train /data/ds001980/comp-bin \
     --lr 0.0001 --clip-norm 0.1 --max-tokens 4000 \
-    --arch transformer_vaswani_wmt_en_de_big --no-epoch-checkpoints --save-dir checkpoints
+    --arch lstm_wiseman_iwslt_de_en --no-epoch-checkpoints --save-dir lcheck
